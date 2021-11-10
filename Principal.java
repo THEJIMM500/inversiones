b724839a8dccb11d0c8c30c0086d2326a0ce0486
rclescolapios <92930770+rclescolapios@users.noreply.github.com>
2021-11-10 14:03:06 +0100

    Update Principal.java
    Refactorización con métodos

--- Principal.java
+++ Principal.java
@@ -4,10 +4,18 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class Principal {
 
 	private JFrame ventanaPrincipal;
+	private JLabel lblUsuario;
+	private JButton btnVender, btnComprar, btnDatosPersonales, btnMovimientos, btnSaldo, btnCerrarSesion;
+	private JPanel panelOpciones;
+	private JPanel panel_1;
 
 	/**
 	 * Launch the application.
@@ -37,66 +45,13 @@ public class Principal {
 	 */
 	private void initialize() {
 		ventanaPrincipal = new JFrame();
-		ventanaPrincipal.setTitle("Principal");
-		ventanaPrincipal.setBounds(100, 100, 740, 430);
-		ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventanaPrincipal.getContentPane().setLayout(null);
+		addVentana();
 		
-		JLabel lblUsuario = new JLabel("Usuario");
-		lblUsuario.setBounds(10, 11, 46, 14);
-		ventanaPrincipal.getContentPane().add(lblUsuario);
+		lblUsuario = new JLabel("Usuario");
+		addLblUsuario();
 		
-		JButton btnDatosPersonales = new JButton("DatosPersonales");
-		btnDatosPersonales.setBounds(10, 82, 89, 23);
-		ventanaPrincipal.getContentPane().add(btnDatosPersonales);
-		btnDatosPersonales.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-		        DatosPersonales datos = new DatosPersonales();
-		        datos.main(null);
-		        ventanaPrincipal.setVisible(false);
-			}
-		});
-		
-		JButton btnMovimientos = new JButton("Movimientos");
-		btnMovimientos.setBounds(10, 137, 89, 23);
-		ventanaPrincipal.getContentPane().add(btnMovimientos);
-		btnMovimientos.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-		        Movimientos movimientos = new Movimientos();
-		        movimientos.main(null);
-		        ventanaPrincipal.setVisible(false);
-			}
-		});
-		
-		JButton btnSaldo = new JButton("Saldo");
-		btnSaldo.setBounds(10, 193, 89, 23);
-		ventanaPrincipal.getContentPane().add(btnSaldo);
-		btnSaldo.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-		        Saldo saldo = new Saldo();
-		        saldo.main(null);
-		        ventanaPrincipal.setVisible(false);
-			}
-		});
-		
-		JButton btnCerrarSesion = new JButton("Cerrar sesi\u00F3n");
-		btnCerrarSesion.setBounds(10, 254, 89, 23);
-		ventanaPrincipal.getContentPane().add(btnCerrarSesion);
-		btnCerrarSesion.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-		        InicioSesion inicio = new InicioSesion();
-		        inicio.main(null);
-		        ventanaPrincipal.setVisible(false);
-			}
-		});
-		
-		JButton btnVender = new JButton("Vender");
-		btnVender.setBounds(625, 233, 89, 23);
-		ventanaPrincipal.getContentPane().add(btnVender);
+		btnVender = new JButton("");
+		addBtnVender();
 		btnVender.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -106,9 +61,8 @@ public class Principal {
 			}
 		});
 		
-		JButton btnComprar = new JButton("Comprar");
-		btnComprar.setBounds(625, 112, 89, 23);
-		ventanaPrincipal.getContentPane().add(btnComprar);
+		btnComprar = new JButton("");
+		addBtnComprar();
 		btnComprar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -116,7 +70,114 @@ public class Principal {
 		        comprar.main(null);
 		        ventanaPrincipal.setVisible(false);
 			}
+		});		
+		
+		panelOpciones = new JPanel();
+		addPanelOpciones();
+		
+		btnDatosPersonales = new JButton("");
+		addBtnDatosPersonales();
+		btnDatosPersonales.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+		        DatosPersonales datos = new DatosPersonales();
+		        datos.main(null);
+		        ventanaPrincipal.setVisible(false);
+			}
 		});
+		
+		btnMovimientos = new JButton("");
+		addBtnMovimientos();
+		btnMovimientos.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+		        Movimientos movimientos = new Movimientos();
+		        movimientos.main(null);
+		        ventanaPrincipal.setVisible(false);
+			}
+		});
+		
+		btnSaldo = new JButton("");
+		addBtnSaldo();
+		btnSaldo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+		        Saldo saldo = new Saldo();
+		        saldo.main(null);
+		        ventanaPrincipal.setVisible(false);
+			}
+		});
+		
+		btnCerrarSesion = new JButton("");
+		addBtnCerrarSesion();
+		btnCerrarSesion.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+		        InicioSesion inicio = new InicioSesion();
+		        inicio.main(null);
+		        ventanaPrincipal.setVisible(false);
+			}
+		});
+		
+		panel_1 = new JPanel();
+		panel_1.setBounds(195, 70, 327, 268);
+		ventanaPrincipal.getContentPane().add(panel_1);
+
 	}
 
+	public void addVentana() {
+		ventanaPrincipal.getContentPane().setBackground(Color.WHITE);
+		ventanaPrincipal.setTitle("Principal");
+		ventanaPrincipal.setBounds(100, 100, 740, 430);
+		ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventanaPrincipal.getContentPane().setLayout(null);
+	}
+
+	public void addBtnCerrarSesion() {
+		btnCerrarSesion.setBackground(Color.WHITE);
+		btnCerrarSesion.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\icon_sign-out.png"));
+		panelOpciones.add(btnCerrarSesion);
+	}
+
+	public void addBtnSaldo() {
+		btnSaldo.setBackground(Color.WHITE);
+		btnSaldo.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\icon_credit-card.png"));
+		panelOpciones.add(btnSaldo);
+	}
+
+	public void addBtnMovimientos() {
+		btnMovimientos.setBackground(Color.WHITE);
+		btnMovimientos.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\icon_exchange.png"));
+		panelOpciones.add(btnMovimientos);
+	}
+
+	public void addBtnDatosPersonales() {
+		btnDatosPersonales.setBackground(Color.WHITE);
+		btnDatosPersonales.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\icon_book.png"));
+		panelOpciones.add(btnDatosPersonales);
+	}
+
+	public void addPanelOpciones() {
+		panelOpciones.setBackground(Color.WHITE);
+		panelOpciones.setBounds(10, 70, 100, 310);
+		ventanaPrincipal.getContentPane().add(panelOpciones);
+	}
+
+	public void addBtnComprar() {
+		btnComprar.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\Comprar.png"));
+		btnComprar.setBackground(Color.WHITE);
+		btnComprar.setBounds(549, 113, 142, 47);
+		ventanaPrincipal.getContentPane().add(btnComprar);
+	}
+
+	public void addBtnVender() {
+		btnVender.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\Vender.png"));
+		btnVender.setBounds(549, 232, 142, 46);
+		ventanaPrincipal.getContentPane().add(btnVender);
+	}
+
+	public void addLblUsuario() {
+		lblUsuario.setBounds(10, 11, 46, 14);
+		ventanaPrincipal.getContentPane().add(lblUsuario);
+	}
 }
