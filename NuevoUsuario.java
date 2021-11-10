660aacec3b1d67729065181f45a79a31f169146f
rclescolapios <92930770+rclescolapios@users.noreply.github.com>
2021-11-10 14:02:09 +0100

    Update NuevoUsuario.java
    Refactorización con métodos

--- NuevoUsuario.java
+++ NuevoUsuario.java
@@ -4,13 +4,14 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.awt.Color;
 
 public class NuevoUsuario {
 
 	private JFrame ventanaRegistro;
-	private JTextField textUsuario;
-	private JTextField textContrasena;
-	private JTextField textConfirmar;
+	private JTextField textUsuario, textContrasena, textConfirmar;
+	private JLabel lblUsuario, lblContrasena, lblConfirmar;
+	private JButton btnCrearCuenta, btnRetroceder;
 
 	/**
 	 * Launch the application.
@@ -40,45 +41,32 @@ public class NuevoUsuario {
 	 */
 	private void initialize() {
 		ventanaRegistro = new JFrame();
-		ventanaRegistro.setTitle("Registro");
-		ventanaRegistro.setBounds(100, 100, 450, 300);
-		ventanaRegistro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventanaRegistro.getContentPane().setLayout(null);
+		addVentana();
 		
-		JLabel lblUsuario = new JLabel("Nombre de usuario");
-		lblUsuario.setBounds(72, 56, 92, 14);
-		ventanaRegistro.getContentPane().add(lblUsuario);
+		lblUsuario = new JLabel("Nombre de usuario");
+		addLblUsuario();
+		//addLabel(lblUsuario, 72, 56, 92, 14);
 		
 		textUsuario = new JTextField();
-		textUsuario.setBounds(72, 76, 283, 20);
-		textUsuario.setColumns(10);
-		ventanaRegistro.getContentPane().add(textUsuario);
+		addTextUsuario();
 		
-		JLabel lblContrasena = new JLabel("Contrase\u00F1a");
-		lblContrasena.setBounds(72, 114, 56, 14);
-		ventanaRegistro.getContentPane().add(lblContrasena);
+		lblContrasena = new JLabel("Contrase\u00F1a");
+		addLblContrasena();
 		
 		textContrasena = new JTextField();
-		textContrasena.setBounds(72, 134, 283, 20);
-		textContrasena.setColumns(10);
-		ventanaRegistro.getContentPane().add(textContrasena);
+		addTextContrasena();
 		
-		JLabel lblConfirmar = new JLabel("Confirma contrase\u00F1a");
-		lblConfirmar.setBounds(72, 172, 102, 14);
-		ventanaRegistro.getContentPane().add(lblConfirmar);
+		lblConfirmar = new JLabel("Confirma contrase\u00F1a");
+		addLblConfirmar();
 		
 		textConfirmar = new JTextField();
-		textConfirmar.setBounds(72, 192, 283, 20);
-		textConfirmar.setColumns(10);
-		ventanaRegistro.getContentPane().add(textConfirmar);
+		addTextConfirmar();
 		
-		JButton btnCrearCuenta = new JButton("Crear cuenta");
-		btnCrearCuenta.setBounds(162, 223, 95, 23);
-		ventanaRegistro.getContentPane().add(btnCrearCuenta);
+		btnCrearCuenta = new JButton("");
+		addBtnCrearCuenta();
 		
-		JButton btnRetroceder = new JButton("Retroceder");
-		btnRetroceder.setBounds(10, 11, 87, 23);
-		ventanaRegistro.getContentPane().add(btnRetroceder);
+		btnRetroceder = new JButton("");
+		addBtnRetroceder();
 		btnRetroceder.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -88,4 +76,65 @@ public class NuevoUsuario {
 			}
 		});
 	}
+
+	public void addVentana() {
+		ventanaRegistro.getContentPane().setBackground(Color.WHITE);
+		ventanaRegistro.setTitle("Registro");
+		ventanaRegistro.setBounds(100, 100, 450, 335);
+		ventanaRegistro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventanaRegistro.getContentPane().setLayout(null);
+	}
+
+	public void addBtnRetroceder() {
+		btnRetroceder.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\Retroceder.png"));
+		btnRetroceder.setBackground(Color.WHITE);
+		btnRetroceder.setBounds(10, 11, 136, 33);
+		ventanaRegistro.getContentPane().add(btnRetroceder);
+	}
+
+	public void addBtnCrearCuenta() {
+		btnCrearCuenta.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\Crear cuenta.png"));
+		btnCrearCuenta.setBackground(Color.WHITE);
+		btnCrearCuenta.setBounds(141, 229, 152, 45);
+		ventanaRegistro.getContentPane().add(btnCrearCuenta);
+	}
+
+	public void addTextConfirmar() {
+		textConfirmar.setBounds(72, 192, 283, 20);
+		textConfirmar.setColumns(10);
+		ventanaRegistro.getContentPane().add(textConfirmar);
+	}
+
+	public void addLblConfirmar() {
+		lblConfirmar.setBounds(72, 172, 150, 14);
+		ventanaRegistro.getContentPane().add(lblConfirmar);
+	}
+
+	public void addTextContrasena() {
+		textContrasena.setBounds(72, 134, 283, 20);
+		textContrasena.setColumns(10);
+		ventanaRegistro.getContentPane().add(textContrasena);
+	}
+
+	public void addLblContrasena() {
+		lblContrasena.setBounds(72, 114, 155, 14);
+		ventanaRegistro.getContentPane().add(lblContrasena);
+	}
+
+	public void addTextUsuario() {
+		textUsuario.setBounds(72, 76, 283, 20);
+		textUsuario.setColumns(10);
+		ventanaRegistro.getContentPane().add(textUsuario);
+	}
+
+	public void addLblUsuario() {
+		lblUsuario.setBounds(72, 56, 150, 14);
+		ventanaRegistro.getContentPane().add(lblUsuario);
+	}
+	
+	public void addLabel(JLabel label, int x, int y, int w, int h) {
+		label.setBounds(x, y, w, h);
+		ventanaRegistro.getContentPane().add(label);
+	}
+	
 }
