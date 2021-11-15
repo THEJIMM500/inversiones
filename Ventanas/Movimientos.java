805df648a81d2fb51a9040e8dc4ea4a8ec00cf73
rclescolapios <92930770+rclescolapios@users.noreply.github.com>
2021-11-15 12:11:17 +0100

    Update Movimientos.java
    Añadida tabla con datos del historial

--- Ventanas/Movimientos.java
+++ Ventanas/Movimientos.java
@@ -3,17 +3,21 @@ package inversiones;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.awt.Color;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.*;
 
 public class Movimientos {
 
 	private JFrame ventanaMovimientos;
-	private JLabel lblEmpresa;
-	private JTextPane textPaneGanancias;
+	private JTable tableHistorial;
+	private ConexionHistorial conexion;
+	private JScrollPane scrollPane;
+	private DefaultTableModel model;
 	private JButton btnRetroceder;
-	private JList listHistorial;
-	private String nombreGuardado;
+	private String nombreGuardado = "ro_cl";
 
 	/**
 	 * Launch the application.
@@ -22,7 +26,7 @@ public class Movimientos {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Movimientos window = new Movimientos();
+					Movimientos window = new Movimientos("ro_cl");
 					window.ventanaMovimientos.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -34,22 +38,29 @@ public class Movimientos {
 	/**
 	 * Create the application.
 	 */
-	public Movimientos() {
+	public Movimientos(String nombreGuardado) throws SQLException {
+		this.nombreGuardado = nombreGuardado;
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() throws SQLException {
+		
 		ventanaMovimientos = new JFrame();
 		addVentana();
+
 		
-		lblEmpresa = new JLabel("Empresa");
-		addLblEmpresa();
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 77, 414, 173);
+		ventanaMovimientos.getContentPane().add(scrollPane);
 		
-		textPaneGanancias = new JTextPane();
-		addTextPaneGanancias();
+		model = new DefaultTableModel();
+		tableHistorial = new JTable(model);
+		addTableHistorial();
+		
+		scrollPane.setViewportView(tableHistorial);
 		
 		btnRetroceder = new JButton("");
 		addBtnRetroceder();
@@ -62,37 +73,47 @@ public class Movimientos {
 			}
 		});
 		
-		listHistorial = new JList();
-		addListHistorial();
-		
 	}
 
-	public void addVentana() {
-		ventanaMovimientos.getContentPane().setBackground(Color.WHITE);
+	private void addVentana() {
 		ventanaMovimientos.setTitle("Movimientos");
+		ventanaMovimientos.getContentPane().setForeground(Color.WHITE);
 		ventanaMovimientos.setBounds(100, 100, 450, 300);
 		ventanaMovimientos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventanaMovimientos.getContentPane().setLayout(null);
 	}
 
-	public void addListHistorial() {
-		listHistorial.setBounds(65, 125, 279, 108);
-		ventanaMovimientos.getContentPane().add(listHistorial);
+	private void addTableHistorial() throws SQLException {
+		tableHistorial.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"ID", "Usuario", "Empresa", "Valor", "Tipo", "Fecha"
+			}
+		));
+		
+		conexion = new ConexionHistorial();
+		ArrayList<Integer> listaIds = conexion.getIds(nombreGuardado);
+		historialCompra nombre = null;
+		int columnas = tableHistorial.getModel().getColumnCount();
+		Object[] fila = new Object[columnas];
+		for (int i = 0; i < listaIds.size(); i++) {
+			nombre = conexion.getHistorial(listaIds.get(i));
+			fila[0] = nombre.getID();
+			fila[1] = nombre.getUsuario();
+			fila[2] = nombre.getEmpresa();
+			fila[3] = nombre.getValorCompra();
+			fila[4] = nombre.getTipoTransacion();
+			fila[5] = nombre.getFecha();
+			((DefaultTableModel) tableHistorial.getModel()).addRow(fila);			
+		}
 	}
-
-	public void addBtnRetroceder() {
-		btnRetroceder.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\Retroceder.png"));
+	
+	private void addBtnRetroceder() {
+		btnRetroceder.setBackground(Color.WHITE);
+		btnRetroceder.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Documents\\DAM\\Programaci\u00F3n\\Eclipse\\eclipse-workspace\\2\u00BAdam\\Retroceder.png"));
 		btnRetroceder.setBounds(10, 11, 136, 33);
+		btnRetroceder.setBorder(null);
 		ventanaMovimientos.getContentPane().add(btnRetroceder);
-	}
-
-	public void addTextPaneGanancias() {
-		textPaneGanancias.setBounds(220, 52, 124, 20);
-		ventanaMovimientos.getContentPane().add(textPaneGanancias);
-	}
-
-	public void addLblEmpresa() {
-		lblEmpresa.setBounds(113, 52, 41, 14);
-		ventanaMovimientos.getContentPane().add(lblEmpresa);
 	}
 }
