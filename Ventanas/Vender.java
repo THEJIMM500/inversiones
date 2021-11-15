5c2372753f167146082ed1e7ba5dc0d888a8c5b4
rclescolapios <92930770+rclescolapios@users.noreply.github.com>
2021-11-15 13:53:15 +0100

    Update Vender.java
    Añadida empresa seleccionada para funcionalidad en botón vender

--- Ventanas/Vender.java
+++ Ventanas/Vender.java
@@ -3,18 +3,21 @@ package inversiones;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
 import javax.swing.*;
 import java.awt.Color;
-import javax.swing.ImageIcon;
 
 public class Vender {
-
+	
+	private ConexionAcciones conexion;
+	private ConexionHistorial historial;
 	private JFrame ventanaVender;
 	private JTextField textCantidad, textValor;
 	private JButton btnCompletar, btnCancelar;
 	private JLabel lblCantidad;
-	private JCheckBox chckbxValor;
-	private String nombreGuardado;
+	private JCheckBox checkValor;
+	private String nombreGuardado, empresaSeleccionada;
 
 	/**
 	 * Launch the application.
@@ -23,7 +26,7 @@ public class Vender {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Vender window = new Vender();
+					Vender window = new Vender(null, null);
 					window.ventanaVender.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -35,7 +38,9 @@ public class Vender {
 	/**
 	 * Create the application.
 	 */
-	public Vender() {
+	public Vender(String nombreGuardado, String empresaSeleccionada) {
+		this.nombreGuardado = nombreGuardado;
+		this.empresaSeleccionada = empresaSeleccionada;
 		initialize();
 	}
 
@@ -44,17 +49,10 @@ public class Vender {
 	 */
 	private void initialize() {
 		ventanaVender = new JFrame();
-		ventanaVender.getContentPane().setBackground(Color.WHITE);
-		ventanaVender.setTitle("Vender");
-		ventanaVender.setBounds(100, 100, 450, 300);
-		ventanaVender.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventanaVender.getContentPane().setLayout(null);
+		addVentana();
 		
 		btnCancelar = new JButton("");
-		btnCancelar.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\Cancelar.png"));
-		btnCancelar.setBackground(Color.WHITE);
-		btnCancelar.setBounds(10, 11, 110, 23);
-		ventanaVender.getContentPane().add(btnCancelar);
+		addBtnCancelar();
 		btnCancelar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -65,29 +63,144 @@ public class Vender {
 		});
 		
 		lblCantidad = new JLabel("Cantidad");
-		lblCantidad.setBounds(120, 74, 60, 14);
-		ventanaVender.getContentPane().add(lblCantidad);
+		addLblCantidad();
 		
 		textCantidad = new JTextField();
-		textCantidad.setBounds(187, 71, 86, 20);
-		textCantidad.setColumns(10);
-		ventanaVender.getContentPane().add(textCantidad);
+		addTextCantidad();
 		
 		textValor = new JTextField();
-		textValor.setBounds(187, 109, 86, 20);
-		textValor.setColumns(10);
-		ventanaVender.getContentPane().add(textValor);
+		addTextValor();
+		
+		checkValor = new JCheckBox("Valor");
+		addCheckValor();
 		
 		btnCompletar = new JButton("");
+		addBtnCompletar();
+		btnCompletar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if (empresaSeleccionada.equals("amazon")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				} else if (empresaSeleccionada.equals("apple")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				} else if (empresaSeleccionada.equals("facebook")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				} else if (empresaSeleccionada.equals("google")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				} else if (empresaSeleccionada.equals("linkedin")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				} else if (empresaSeleccionada.equals("twitch")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				} else if (empresaSeleccionada.equals("microsoft")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				} else if (empresaSeleccionada.equals("git")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				} else if (empresaSeleccionada.equals("paypal")) {
+					try {
+						conexion.deleteOperacion(empresaSeleccionada, nombreGuardado);
+						historial.insertaTransaccion(ID, nombreGuardado, empresaSeleccionada, textCantidad.getText(), "venta");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				}
+				
+			}
+		});
+		
+	}
+
+	private void addVentana() {
+		ventanaVender.getContentPane().setBackground(Color.WHITE);
+		ventanaVender.setTitle("Vender");
+		ventanaVender.setBounds(100, 100, 450, 300);
+		ventanaVender.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventanaVender.getContentPane().setLayout(null);
+	}
+
+	private void addCheckValor() {
+		checkValor.setBackground(Color.WHITE);
+		checkValor.setBounds(120, 106, 60, 23);
+		ventanaVender.getContentPane().add(checkValor);
+	}
+
+	private void addBtnCompletar() {
 		btnCompletar.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\Completar.png"));
 		btnCompletar.setBackground(Color.WHITE);
 		btnCompletar.setBounds(154, 177, 127, 42);
 		ventanaVender.getContentPane().add(btnCompletar);
-		
-		chckbxValor = new JCheckBox("Valor");
-		chckbxValor.setBackground(Color.WHITE);
-		chckbxValor.setBounds(120, 106, 60, 23);
-		ventanaVender.getContentPane().add(chckbxValor);
-		
+	}
+
+	private void addTextValor() {
+		textValor.setBounds(187, 109, 86, 20);
+		textValor.setColumns(10);
+		ventanaVender.getContentPane().add(textValor);
+	}
+
+	private void addTextCantidad() {
+		textCantidad.setBounds(187, 71, 86, 20);
+		textCantidad.setColumns(10);
+		ventanaVender.getContentPane().add(textCantidad);
+	}
+
+	private void addLblCantidad() {
+		lblCantidad.setBounds(120, 74, 60, 14);
+		ventanaVender.getContentPane().add(lblCantidad);
+	}
+
+	private void addBtnCancelar() {
+		btnCancelar.setIcon(new ImageIcon("C:\\Users\\ro_cl\\Downloads\\Cancelar.png"));
+		btnCancelar.setBackground(Color.WHITE);
+		btnCancelar.setBounds(10, 11, 110, 23);
+		ventanaVender.getContentPane().add(btnCancelar);
 	}
 }
